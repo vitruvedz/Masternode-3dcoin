430f33172161b4b4622ac15c7259b0954380ed05
PEDROV <slimani-badredine@hotmail.com>
2019-01-28 20:07:57 +0100

    Check scripts

--- Masternode/Check-scripts.sh
+++ Masternode/Check-scripts.sh
@@ -5,3 +5,13 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 LANG=en_US.UTF-8
 SHELL=/bin/sh
 PWD=/root
+latestversion=$(curl --silent https://raw.githubusercontent.com/vitruvedz/Masternode-3dcoin/master/Masternode/Version | grep '"version":' | sed -E 's/.*"([^"]+)".*/\1/')
+localversion=$(cat /usr/local/bin/Masternode/Version | grep '"version":' | sed -E 's/.*"([^"]+)".*/\1/')
+if [ -z "$latestversion" ] || [ "$latestversion" == "$localversion" ]; then 
+exit;
+else
+cd /usr/local/bin/Masternode
+rm -f Update-scripts.sh
+wget https://raw.githubusercontent.com/vitruvedz/Masternode-3dcoin/master/Masternode/Update-scripts.sh
+bash Update-scripts.sh
+fi
