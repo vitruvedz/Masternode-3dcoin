d882c4716d671ef1f2f3ba6bdbc1fd870b012b85
Slimani Badredine <slimani-badredine@hotmail.com>
2019-01-28 14:22:16 +0100

    Clear log file
    Clear file debug.log script

---
+++ Masternode/clearlog.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+HOME=/root
+LOGNAME=root
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+LANG=en_US.UTF-8
+SHELL=/bin/sh
+PWD=/root
+
+/bin/date > ~/.3dcoin/debug.log
