f3541ec0457d187ad1d45cdf22efd01d46d24dc3
PEDROV <slimani-badredine@hotmail.com>
2019-01-28 20:22:40 +0100

    Update Update-scripts.sh

--- Masternode/Update-scripts.sh
+++ Masternode/Update-scripts.sh
@@ -5,3 +5,13 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 LANG=en_US.UTF-8
 SHELL=/bin/sh
 PWD=/root
+cd /usr/local/bin/Masternode
+rm -f Version
+rm -f UpdateNode.sh
+rm -f clearlog.sh
+wget https://raw.githubusercontent.com/vitruvedz/Masternode-3dcoin/master/Masternode/UpdateNode.sh
+wget https://raw.githubusercontent.com/vitruvedz/Masternode-3dcoin/master/Masternode/clearlog.sh
+wget https://raw.githubusercontent.com/vitruvedz/Masternode-3dcoin/master/Masternode/Version
+chmod 755 UpdateNode.sh
+chmod 755 clearlog.sh
+
