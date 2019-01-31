24434e0ed59475fd46638cd1b695df885273dce0
PEDROV <slimani-badredine@hotmail.com>
2019-01-31 03:09:43 +0100

    Add Update Log
    include update log in script to check if there is any problem when update

--- Masternode/UpdateNode.sh
+++ Masternode/UpdateNode.sh
@@ -6,41 +6,43 @@ LANG=en_US.UTF-8
 SHELL=/bin/sh
 PWD=/root
 LOGFILE='/usr/local/bin/Masternode/update.log'
-dt=`date '+%d/%m/%Y_%H:%M:%S'`
+dt=`date '+%d/%m/%Y %H:%M:%S'`
 latestrelease=$(curl --silent https://api.github.com/repos/BlockchainTechLLC/3dcoin/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 localrelease=$(3dcoin-cli -version | awk -F' ' '{print $NF}' | cut -d "-" -f1)
 if [ -z "$latestrelease" ] || [ "$latestrelease" == "$localrelease" ]; then 
-echo "[$dt] : There is no New Update latest release is $latestrelease" >> $LOGFILE
+echo "[$dt]    ==============================================================" >> $LOGFILE
+echo "[$dt]==> Info: There is no New Update latest release is $latestrelease" >> $LOGFILE
+echo "[$dt]    ==============================================================" >> $LOGFILE
 exit;
 else
-echo "[$dt] : Starting Update $latestrelease" >> $LOGFILE
-echo "[$dt] : apt install unzip" >> $LOGFILE
-apt install unzip
+echo "[$dt]    ==============================================================" >> $LOGFILE
+echo "[$dt]==> Info: Starting Update 3DCoin core to $latestrelease" >> $LOGFILE
+echo "[$dt]    ==============================================================" >> $LOGFILE
+echo "[$dt]==> Info: Install unzip" >> $LOGFILE
+apt-get update
+apt-get install unzip || { echo "[$dt]==> Error: When install unzip" >> $LOGFILE && exit;  }
 cd ~
 localfile=${localrelease//[Vv]/3dcoin-}
-echo "[$dt] : remove $localfile" >> $LOGFILE
+echo "[$dt]==> Info: Remove file $localfile" >> $LOGFILE
 rm -rf $localfile
 link="https://github.com/BlockchainTechLLC/3dcoin/archive/$latestrelease.zip"
-echo "[$dt] : Download Last Update $link" >> $LOGFILE
-wget $link
-echo "[$dt] : Unzip $latestrelease.zip" >> $LOGFILE
-unzip $latestrelease.zip
+echo "[$dt]==> Info: Download Last Update $link" >> $LOGFILE
+wget $link  || { echo "[$dt]==> Error: When Download $link" >> $LOGFILE && exit;  }
+echo "[$dt]==> Info: Unzip $latestrelease.zip" >> $LOGFILE
+unzip $latestrelease.zip || { echo "[$dt]==> Error: When unzip $latestrelease.zip" >> $LOGFILE && exit;  }
 file=${latestrelease//[Vv]/3dcoin-}
-cd $file
-echo "[$dt] : Stop 3DCoin core $localrelease" >> $LOGFILE
-3dcoin-cli stop
+cd $file  || { echo "[$dt]==> Error: File $file not found" >> $LOGFILE && exit;  }
+echo "[$dt]==> Info: Stop 3DCoin core $localrelease" >> $LOGFILE
+3dcoin-cli stop || { echo "[$dt]==> Error: 3DCoin core not running but we continue installation" >> $LOGFILE;  }
 sleep 10
-echo "[$dt] : autogen.sh" >> $LOGFILE
-./autogen.sh
-echo "[$dt] : configure" >> $LOGFILE
-./configure --disable-tests --disable-gui-tests --without-gui
-echo "[$dt] : make install-strip" >> $LOGFILE
-make install-strip
+echo "[$dt]==> Info: Start Compiling 3Dcoin core $latestrelease" >> $LOGFILE
+./autogen.sh && ./configure --disable-tests --disable-gui-tests --without-gui && make install-strip || { echo "[$dt]==> Error: When Compiling 3Dcoin core" >> $LOGFILE && exit;  }
 cd ~
-echo "[$dt] : remove $latestrelease.zip" >> $LOGFILE
+echo "[$dt]==> Info: Remove $latestrelease.zip" >> $LOGFILE
 rm $latestrelease.zip
-echo "[$dt] : remove $file" >> $LOGFILE
+echo "[$dt]==> Info: Remove $file" >> $LOGFILE
 rm -rf $file
-echo "[$dt] : rebooting " >> $LOGFILE  
+echo "[$dt]==> Info: Rebooting " >> $LOGFILE 
+echo "[$dt]    ==============================================================" >> $LOGFILE
 reboot
 fi
