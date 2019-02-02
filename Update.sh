0c2fd2204d6f58d4b5b4e343d53098b6e718ef60
PEDROV <slimani-badredine@hotmail.com>
2019-02-02 05:31:01 +0100

    random time
    add random time to update every node

--- Update.sh
+++ Update.sh
@@ -1,7 +1,7 @@
 #####################################################################################
 #                           Update 3DCoin Core V 0.14 	                            #
 #####################################################################################
-RED='\033[0;41;30m'
+RED='\033[0;97;41m'
 STD='\033[0;0;39m'
 GREEN='\e[1;97;42m'
 BLUE='\e[1;97;44m'
@@ -43,6 +43,7 @@ read_options(){
 		#### 3Dcoin Single Node installation
 		if [ "$opt" == "Update Single Masternode" ]; then
 			echo ""
+			h=$(( RANDOM % 23 + 1 ));
 			echo  -e "${BLUE} Start ${Update}                    ${STD}"
 			rm -f /usr/local/bin/check.sh
 			rm -f /usr/local/bin/update.sh
@@ -50,6 +51,8 @@ read_options(){
 			rm -rf /usr/local/bin/Masternode
 			echo ""
 			cd ~
+			apt-get update
+			apt-get install unzip || { echo "Error: When install unzip" && exit;  }
 			echo  -e "${GREEN} Get latest release                ${STD}"
 			latestrelease=$(curl --silent https://api.github.com/repos/BlockchainTechLLC/3dcoin/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 			link="https://github.com/BlockchainTechLLC/3dcoin/archive/$latestrelease.zip"
@@ -61,14 +64,12 @@ read_options(){
 			sudo /etc/init.d/cron stop
 			echo ""
 			echo  -e "${GREEN} Stop 3Dcoin core                  ${STD}"
-			3dcoin-cli stop
+			3dcoin-cli stop || { echo "Error: 3DCoin core not running but we continue installation";  }
 			sleep 10
 			echo ""		
 			echo  -e "${GREEN} Make install                      ${STD}"
 			cd $file
-			./autogen.sh
-			./configure --disable-tests --disable-gui-tests --without-gui
-			make install-strip
+			./autogen.sh && ./configure --disable-tests --disable-gui-tests --without-gui && make install-strip || { echo "Error: When Compiling 3Dcoin core" && exit && 3dcoind;  }
 			sleep 10
 			echo ""
 			echo  -e "${GREEN} Update crontab                    ${STD}"
@@ -81,8 +82,6 @@ read_options(){
 			wget https://raw.githubusercontent.com/vitruvedz/Masternode-3dcoin/master/Masternode/UpdateNode.sh
 			wget https://raw.githubusercontent.com/vitruvedz/Masternode-3dcoin/master/Masternode/clearlog.sh
 			wget https://raw.githubusercontent.com/vitruvedz/Masternode-3dcoin/master/Masternode/Version
-			wget https://raw.githubusercontent.com/vitruvedz/Masternode-3dcoin/master/Masternode/update.log
-            chmod 755 update.log
             chmod 755 UpdateNode.sh
             chmod 755 Check-scripts.sh
             chmod 755 Update-scripts.sh
@@ -91,7 +90,7 @@ read_options(){
             crontab -r
             line="@reboot /usr/local/bin/3dcoind
 0 0 * * * /usr/local/bin/Masternode/Check-scripts.sh
-0 1 * * * /usr/local/bin/Masternode/UpdateNode.sh
+0 $h * * * /usr/local/bin/Masternode/UpdateNode.sh
 * * */2 * * /usr/local/bin/Masternode/clearlog.sh"
 			echo "$line" | crontab -u root -
 			echo "Crontab updated successfully"
@@ -110,44 +109,51 @@ read_options(){
 			echo ""
 			echo  -e "${BLUE} Start ${Update}                    ${STD}"
 			echo ""
-			echo  -e "${GREEN} Enter Vps ip's                    ${STD}"
+			
+			read -p "Same SSH Port and Password for all vps's? (Y/N)" -n 1 -r
+			echo ""   # (optional) move to a new line
+			if [[ $REPLY =~ ^[Yy]$ ]]; then
 			echo ""
-			echo  -e "Please enter your vps ip's: ${RED}(Exemple: 111.111.111.111 222.222.222.222 ) ${STD}"
+			echo  -e "${GREEN} Enter Vps ip's                    ${STD}"
+			echo ""			
+			sleep 2
+			echo  -e "Please enter your vps ip's: ${RED}(Exemple: 111.111.111.111-222.222.222.222-... ) ${STD}"
 			
 			unset ip
 			while [ -z ${ip} ]; do
-				read -p "IP HERE: " ip
+			read -p "IP HERE: " ip
 			done
+
+			unset sshport
+			while [ -z ${sshport} ]; do
+			read -p "SSH Port: " sshport
+			done
+
+			unset rootpass
+			while [ -z ${rootpass} ]; do
+			read -s -p "Password: " rootpass
+			done
+			echo ""
+			
+			vpsip=$(echo $ip | tr "-" " ")
 			
 			apt-get update
 			yes | apt-get install sshpass
-			for i in $ip
+			for i in $vpsip
 			do
-				echo  -e "${GREEN} Connexion Vps ip $i               ${STD}"
-				echo ""
-				
-				unset rootpass
-				while [ -z ${rootpass} ]; do
-					read -s -p "Please Enter Password Root $i: " rootpass
-				done
-				
-				echo  -e "${STD}"
-				echo ""
-				echo  -e "${GREEN} SSH port Vps ip $i               ${STD}"
-				echo ""
-				
-				unset sshport
-				while [ -z ${sshport} ]; do
-					read -s -p "Please Enter ssh port $i: " sshport
-				done
+			echo  -e "${GREEN} Connexion Vps ip $i               ${STD}"
+			echo ""
 		
         sshpass -p $rootpass ssh -p$sshport -o StrictHostKeyChecking=no root@$i '
+		h=$(( RANDOM % 23 + 1 ));
         rm -f /usr/local/bin/check.sh
         rm -f /usr/local/bin/update.sh
         rm -f /usr/local/bin/UpdateNode.sh
 		rm -rf /usr/local/bin/Masternode
-        echo ""
-        echo "-----------------------------------------------------"
+		apt-get update
+		apt-get install unzip || { echo "Error: When install unzip" && exit;  }
+		cd ~
+		echo "-----------------------------------------------------"
         echo  -e "${GREEN} Git checkout master               ${STD}"
         echo "-----------------------------------------------------"
         latestrelease=$(curl --silent https://api.github.com/repos/BlockchainTechLLC/3dcoin/releases/latest | grep '"'"\\$"\"tag_name"\\$"\":'"' | sed -E '"'s/.*"\\$"\"([^"\\$"\"]+)"\\$"\".*/\1/'"')
@@ -164,16 +170,14 @@ read_options(){
         echo "-----------------------------------------------------"
         echo  -e "${GREEN} Stop 3Dcoin core                  ${STD}"
         echo "-----------------------------------------------------"
-        cd $file
-        3dcoin-cli stop
+		3dcoin-cli stop || { echo "Error: 3DCoin core not running but we continue installation";  }
 		sleep 10
         echo ""		
         echo "-----------------------------------------------------"
         echo  -e "${GREEN} Make install                      ${STD}"
         echo "-----------------------------------------------------"
-        ./autogen.sh 
-		./configure --disable-tests --disable-gui-tests --without-gui 
-		make install-strip
+        cd $file
+		./autogen.sh && ./configure --disable-tests --disable-gui-tests --without-gui && make install-strip || { echo "Error: When Compiling 3Dcoin core" && exit && 3dcoind;  }
 		sleep 10
         echo ""
         echo "-----------------------------------------------------"
@@ -188,8 +192,6 @@ read_options(){
 		wget https://raw.githubusercontent.com/vitruvedz/Masternode-3dcoin/master/Masternode/UpdateNode.sh
 		wget https://raw.githubusercontent.com/vitruvedz/Masternode-3dcoin/master/Masternode/clearlog.sh
 		wget https://raw.githubusercontent.com/vitruvedz/Masternode-3dcoin/master/Masternode/Version
-        wget https://raw.githubusercontent.com/vitruvedz/Masternode-3dcoin/master/Masternode/update.log
-        chmod 755 update.log
 		chmod 755 UpdateNode.sh
         chmod 755 Check-scripts.sh
         chmod 755 Update-scripts.sh
@@ -198,7 +200,7 @@ read_options(){
         crontab -r
         line="@reboot /usr/local/bin/3dcoind
 0 0 * * * /usr/local/bin/Masternode/Check-scripts.sh
-0 1 * * * /usr/local/bin/Masternode/UpdateNode.sh
+0 $h * * * /usr/local/bin/Masternode/UpdateNode.sh
 * * */2 * * /usr/local/bin/Masternode/clearlog.sh"
         echo "$line" | crontab -u root -
         echo "Crontab updated successfully"
@@ -211,7 +213,106 @@ read_options(){
 		rm -rf $file 
         reboot'
         done
+		
+	elif [[ $REPLY =~ ^[Nn]$ ]]; then
+	sleep 2
+	echo  -e "Please enter your vps's data: 'Host:Password:SSHPort' ${RED}( Exemple: 111.111.111.111:ERdX5h64dSer:22-222.222.222.222:Wz65D232Fty:165-... )${STD}"
 
+	unset ip
+	while [ -z ${ip} ]; do
+	read -p "DATA HERE: " ip
+	done
+
+	apt-get update
+	yes | apt-get install sshpass
+			
+	data=$(echo $ip | tr "-" " ")
+	for i in $data
+	do
+	vpsdata=$(echo $i | tr ":" "\n")
+	declare -a array=($vpsdata)
+	host=${array[0]}
+	pass=${array[1]}
+	port=${array[2]}
+	if [ -z "$pass" ] || [ -z "$port" ] || [ -z "$host" ]
+	then
+	echo -e "Please enter a correct vps's data ${RED}( Exemple: 111.111.111.111:ERdX5h64dSer:22 222.222.222.222:Wz65D232Fty:165 ... )${STD}"
+	else
+	
+	sshpass -p $pass ssh -p$port -o StrictHostKeyChecking=no root@$host '
+		h=$(( RANDOM % 23 + 1 ));
+        rm -f /usr/local/bin/check.sh
+        rm -f /usr/local/bin/update.sh
+        rm -f /usr/local/bin/UpdateNode.sh
+		rm -rf /usr/local/bin/Masternode
+		apt-get update
+		apt-get install unzip || { echo "Error: When install unzip" && exit;  }
+		cd ~
+		echo "-----------------------------------------------------"
+        echo  -e "${GREEN} Git checkout master               ${STD}"
+        echo "-----------------------------------------------------"
+        latestrelease=$(curl --silent https://api.github.com/repos/BlockchainTechLLC/3dcoin/releases/latest | grep '"'"\\$"\"tag_name"\\$"\":'"' | sed -E '"'s/.*"\\$"\"([^"\\$"\"]+)"\\$"\".*/\1/'"')
+        link="https://github.com/BlockchainTechLLC/3dcoin/archive/$latestrelease.zip"
+        wget $link
+        unzip $latestrelease.zip
+        file=${latestrelease//[Vv]/3dcoin-}
+        echo ""
+        echo "-----------------------------------------------------"
+        echo  -e "${GREEN} Stop Cron                         ${STD}"
+        echo "-----------------------------------------------------"
+        sudo /etc/init.d/cron stop
+        echo ""
+        echo "-----------------------------------------------------"
+        echo  -e "${GREEN} Stop 3Dcoin core                  ${STD}"
+        echo "-----------------------------------------------------"
+		3dcoin-cli stop || { echo "Error: 3DCoin core not running but we continue installation";  }
+		sleep 10
+        echo ""		
+        echo "-----------------------------------------------------"
+        echo  -e "${GREEN} Make install                      ${STD}"
+        echo "-----------------------------------------------------"
+        cd $file
+		./autogen.sh && ./configure --disable-tests --disable-gui-tests --without-gui && make install-strip || { echo "Error: When Compiling 3Dcoin core" && exit && 3dcoind;  }
+		sleep 10
+        echo ""
+        echo "-----------------------------------------------------"
+        echo  -e "${GREEN} Update crontab                    ${STD}"
+        echo "-----------------------------------------------------" 
+        cd ~
+        cd /usr/local/bin
+		mkdir Masternode
+		cd Masternode
+		wget https://raw.githubusercontent.com/vitruvedz/Masternode-3dcoin/master/Masternode/Check-scripts.sh
+		wget https://raw.githubusercontent.com/vitruvedz/Masternode-3dcoin/master/Masternode/Update-scripts.sh
+		wget https://raw.githubusercontent.com/vitruvedz/Masternode-3dcoin/master/Masternode/UpdateNode.sh
+		wget https://raw.githubusercontent.com/vitruvedz/Masternode-3dcoin/master/Masternode/clearlog.sh
+		wget https://raw.githubusercontent.com/vitruvedz/Masternode-3dcoin/master/Masternode/Version
+		chmod 755 UpdateNode.sh
+        chmod 755 Check-scripts.sh
+        chmod 755 Update-scripts.sh
+        chmod 755 clearlog.sh
+        cd ~
+        crontab -r
+        line="@reboot /usr/local/bin/3dcoind
+0 0 * * * /usr/local/bin/Masternode/Check-scripts.sh
+0 $h * * * /usr/local/bin/Masternode/UpdateNode.sh
+* * */2 * * /usr/local/bin/Masternode/clearlog.sh"
+        echo "$line" | crontab -u root -
+        echo "Crontab updated successfully"
+        echo "" 
+        echo "-----------------------------------------------------"
+	    echo  -e "${GREEN} Update Finished,rebooting server  ${STD}" 
+        echo "-----------------------------------------------------"
+		cd ~
+        rm $latestrelease.zip
+		rm -rf $file 
+        reboot'
+		fi
+		done
+		else
+		exit;
+		fi
+		
 		fi
         exit 0;;
 		
@@ -235,6 +336,7 @@ read_options(){
 		#### 3Dcoin Single Node installation
 		if [ "$opt" == "Update Single Masternode" ]; then
 			echo ""
+            h=$(( RANDOM % 23 + 1 ));
 			echo  -e "${BLUE} Start ${Update}                    ${STD}"
 			rm -f /usr/local/bin/check.sh
 			rm -f /usr/local/bin/update.sh
@@ -255,8 +357,6 @@ read_options(){
 			wget https://raw.githubusercontent.com/vitruvedz/Masternode-3dcoin/master/Masternode/UpdateNode.sh
 			wget https://raw.githubusercontent.com/vitruvedz/Masternode-3dcoin/master/Masternode/clearlog.sh
 			wget https://raw.githubusercontent.com/vitruvedz/Masternode-3dcoin/master/Masternode/Version
-			wget https://raw.githubusercontent.com/vitruvedz/Masternode-3dcoin/master/Masternode/update.log
-            chmod 755 update.log
 			chmod 755 UpdateNode.sh
 			chmod 755 Check-scripts.sh
 			chmod 755 Update-scripts.sh
@@ -265,7 +365,7 @@ read_options(){
             crontab -r
         line="@reboot /usr/local/bin/3dcoind
 0 0 * * * /usr/local/bin/Masternode/Check-scripts.sh
-0 1 * * * /usr/local/bin/Masternode/UpdateNode.sh
+0 $h * * * /usr/local/bin/Masternode/UpdateNode.sh
 * * */2 * * /usr/local/bin/Masternode/clearlog.sh"
 			echo "$line" | crontab -u root -
 			echo "Crontab updated successfully"
@@ -294,6 +394,7 @@ read_options(){
 		yes | apt-get install sshpass
         for i in $ip
         do
+        h=$(( RANDOM % 23 + 1 ));
         echo  -e "${GREEN} Connexion Vps ip $i               ${STD}"
         echo ""
 		unset rootpass
@@ -333,8 +434,6 @@ read_options(){
 			wget https://raw.githubusercontent.com/vitruvedz/Masternode-3dcoin/master/Masternode/UpdateNode.sh
 			wget https://raw.githubusercontent.com/vitruvedz/Masternode-3dcoin/master/Masternode/clearlog.sh
 			wget https://raw.githubusercontent.com/vitruvedz/Masternode-3dcoin/master/Masternode/Version
-			wget https://raw.githubusercontent.com/vitruvedz/Masternode-3dcoin/master/Masternode/update.log
-            chmod 755 update.log
 			chmod 755 UpdateNode.sh
 			chmod 755 Check-scripts.sh
 			chmod 755 Update-scripts.sh
@@ -343,7 +442,7 @@ read_options(){
 			crontab -r
         line="@reboot /usr/local/bin/3dcoind
 0 0 * * * /usr/local/bin/Masternode/Check-scripts.sh
-0 1 * * * /usr/local/bin/Masternode/UpdateNode.sh
+0 $h * * * /usr/local/bin/Masternode/UpdateNode.sh
 * * */2 * * /usr/local/bin/Masternode/clearlog.sh"
 			echo "$line" | crontab -u root -
 			echo "Crontab updated successfully"
